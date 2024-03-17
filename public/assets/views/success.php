122e9f8633b452bb36eab5b2222c99e21ef562ed
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-03-17 17:23:04 -0400

    Update success.php

--- public/assets/views/success.php
+++ public/assets/views/success.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Posts</title>
+</head>
 
+<body>
+    <h2>success for Posts</h2>
+    
+    <?php
+        if (isset($_GET['success'])) 
+        {
+            echo "<p>Post has been created!</p>";
+        }
+    ?>
+
+    <?php foreach ($posts as $post): ?>
+        <div>
+            <h3>
+                <?php echo $post['name']; ?>
+            </h3>
+
+            <p><?php echo $post['description']; ?></p>
+        </div>
+    <?php endforeach; ?>
+
+</body>
+</html>
