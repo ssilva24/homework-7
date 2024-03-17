5a7ced6b7f8588347df0b3826df1ccc6121dee43
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-03-17 17:28:39 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,8 +4,24 @@ namespace app\models;
 
 class Post
 {
+    private $data;
+    private $details;
         public static function all()
         {
-            return [];
+            return 
+            [
+                ['id' => 1, 'title' => 'Post 1', 'content' => 'post1 content'],
+                ['id' => 2, 'title' => 'Post 2', 'content' => 'post2 content'],
+            ];
         }
+
+    public function setName($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setDetails($details)
+    {
+        $this->details = $details;
+    }
 }
