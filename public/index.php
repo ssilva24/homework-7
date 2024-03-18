a417b56ec819c042094c81dd762ae6b63188d4cd
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-03-18 19:44:33 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require_once "../app/models/Post.php";
 require_once "../app/controllers/MainController.php";
 require_once "../app/controllers/UserController.php";
 require_once "../app/controllers/PostController.php";
-require_once "../public/assets/main/postForm.html";
+// require_once "../public/assets/main/postForm.html";
 use app\controllers\MainController;
 use app\controllers\UserController;
 use app\controllers\PostController;
